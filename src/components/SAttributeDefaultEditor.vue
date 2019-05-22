8a830b62b1832083f67e3f6c4f5c72e715932f22
esvit <esvit666@gmail.com>
2019-05-22 11:35:21 +0300

    fixes

--- src/components/SAttributeDefaultEditor.vue
+++ src/components/SAttributeDefaultEditor.vue
@@ -9,6 +9,16 @@
                 :value="value" @input="$emit('input', $event)"></v-text-field>
 
         </template>
+        <template v-else-if="type === DataTypes.PARAGRAPH_TYPE">
+
+            <v-textarea
+                :rules="required ? [emptyValidation()] : []"
+                :value="value" @input="$emit('input', $event)"></v-textarea>
+
+        </template>
+        <template v-else-if="type === DataTypes.RICHTEXT_TYPE">
+
+        </template>
         <template v-else>
 
             <div class="error--text">No editor for type: {{type}}</div>
